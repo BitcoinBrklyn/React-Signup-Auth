2e25a74947baccbf25bb36615550804cd4dafc8c
BitcoinBrklyn <vincentmollins@rocketmail.com>
2023-11-03 21:59:36 -0400

    renew home welcome dashboard

--- react-signup-auth/src/componets/Home.jsx
+++ react-signup-auth/src/componets/Home.jsx
@@ -3,10 +3,23 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 function Home() {
   const location = useLocation();
-
   return (
-    <div className="homepage">
-      <h1>Hello {location.state.id} and welcome to the home dashboard</h1>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center">Welcome to the Dashboard</h1>
+              <p className="card-text text-center">Hello, {location.state.id}!</p>
+              <div className="text-center">
+                <a href="#" className="btn btn-primary">
+                  Get Started
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
